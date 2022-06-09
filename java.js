98db62f16c8ea852812d34f6bdb436cacbeaf439
anton <anton.pavlysh@gmail.com>
2022-06-09 15:41:12 +0800

    first commit

---
+++ java.js
@@ -0,0 +1,92 @@
+//var num1 = 20;
+//var num2 = 15;
+//num2 = num2 + 10;
+//document.write ("результат: " + num1 + num2 + "</br>");
+//document.write(Math.round(2.44));
+
+
+//var colors = ["red", "blue", "green"];
+//document.write(colors[2]);
+
+//var Cars = ['volvo', 'bmw', 'mers'];
+//Cars.push('btc');
+//document.write(Cars[3]+ ' ' + 'sixy');
+
+//for (var i = 100; i > 10; i -= 20) {
+//    console.log(i);
+//}
+
+//var cars = new Array ('volvo', 'bmw', 'mers');
+//for (var i = 0; i < cars.length; i++)
+//    console.log(cars[i]);
+// var num1 = 20;
+// var num2 = 15;
+// if (num1 > num2){
+//     console.log(num1 + '>' + num2);
+// }
+// else if (num1 <br num2){
+//     console.log("jope");
+// }
+// else {
+//     console.log("all good");
+// }
+
+// var bool = true;
+// if (!bool || num1 == 20) {
+//     console.log(bool + " is true");
+// }
+
+// var res = 5;
+// switch (res) {
+//     case 1:
+//         alert("res is 1");
+//         break;
+//     case 2:
+//         alert("res is 2");
+//         break;
+//     case 3:
+//         alert("res is 3");
+//         break;
+//     default:
+//         alert('res is sucks');
+//         break;
+// }
+// function write (words){
+//     document.write (words);
+// }
+// write ("hi");
+// write ("</br>");
+// write ("hu");
+// function sum (a, b){
+//     return a + b;
+// }
+// document.write (sum(35, 77));
+
+// const answer = +prompt('Hi');
+// console.log(answer + 5);
+
+// const answers = [];
+// // answers[0] = prompt('Nama?', ' ');
+// // answers[1] = prompt('Family name?', ' ');
+// // answers[2] = prompt('Age?', ' ');
+
+// console.log(typeof(null));
+// const category = 'toys';
+// console.log(`https://where.com/${category}/5/fff/56`);
+
+// const name = 'ivan';
+// prompt(`hello, ${name}`);
+console.log(4 + '5');
+
+let incr = 10,
+    decr = 10;
+incr++;
+decr--;
+console.log(incr);
+console.log(decr);
+
+console.log(2*4 == 8);
+
+const isChecked = false,
+isClos = false;
+console.log(isChecked || !isClos);
